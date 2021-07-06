5673bb1c032854f10aa1c76e3cf4f7a022cdc406
Tanishq555 <86484860+Tanishq555@users.noreply.github.com>
2021-07-06 14:47:25 +0530

    Add files via upload

---
+++ demo6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int a,b;
+cin>>a;
+
+
+for (int i =1; i<=a; i++)
+{
+   for (int j=1; j<=a+1-i; j++)
+   {
+   cout<<j<<"  ";
+
+
+   }cout<<endl;
+   
+}return 0;
+
+
+
+
+
+}
